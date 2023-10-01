71e6c0d775767d4e5648692f117809b614ca4f4d
saivelamuri <saivelamuri26@gmail.com>
2023-10-01 13:53:38 +0200

    Main.java updated

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
 public static void main(String[] args){
+
+int a = 74 ;
+int b = 36;
+int sum = a+b;
+
+
+
 System.out.println("Hello World!");
+System.out.println(sum);
+
+
 }
 }
